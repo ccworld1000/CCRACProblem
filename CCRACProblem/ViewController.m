e0e93885eb2a5904230d1e0b681539994cc2dfa2
CC (deng you hua) <ccworld1000@gmail.com>
2021-05-07 13:40:14 +0800

    CCAutoTag : ok

--- CCRACProblem/ViewController.m
+++ CCRACProblem/ViewController.m
@@ -14,13 +14,15 @@
 @property (weak, nonatomic) IBOutlet UITextField *textField2;
 @property (weak, nonatomic) IBOutlet UITextField *textField3;
 
+@property (copy, nonatomic) NSString *testString;
+
 @end
 
 @implementation ViewController
 
 - (void)test1 {
     [self.textField1.rac_textSignal subscribeNext:^(NSString * _Nullable x) {
-        NSLog(@"1 -> %@", x);
+        NSLog(@"1 -> [%@]", x);
     }];
     
     self.textField1.text = @"CC 1";
@@ -28,7 +30,7 @@
 
 - (void)test2 {
     [self.textField2.rac_textSignal subscribeNext:^(NSString * _Nullable x) {
-        NSLog(@"2 -> %@", x);
+        NSLog(@"2 -> [%@]", x);
     }];
     
     [self.textField2 becomeFirstResponder];
@@ -36,11 +38,9 @@
 }
 
 - (void)test3 {
-    [self.textField3.rac_textSignal subscribeNext:^(NSString * _Nullable x) {
-        NSLog(@"3 -> %@", x);
+    [[self.textField3.rac_textSignal merge:RACChannelTo(self.textField3, text)] subscribeNext:^(id  _Nullable x) {
+        NSLog(@"3 -> [%@]", x);
     }];
-    
-    [self.textField3 becomeFirstResponder];
     self.textField3.text = @"CC 3";
 }
 
@@ -48,8 +48,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-//    [self test1];
-//    [self test2];
+    [self test1];
+    [self test2];
     [self test3];
 }
 
